d1085ac856c18cb2211a5b8a568b97a2229a3293
xkwg <xkwg@163.com>
2018-03-10 00:59:29 +0800

    mybatis generator

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.demo.entity.User;
 import com.example.demo.model.UserModel;
+import com.example.demo.pojo.User;
 import com.example.demo.utils.Md5Util;
 
 @RestController
